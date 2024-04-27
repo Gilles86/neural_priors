339861f038f1371f8ef962adfdd71145d2048412
Gilles de Hollander <gilles.de.hollander@gmail.com>
2024-04-27 17:33:34 +0200

    wip

--- neural_priors/glm/fit_single_trials_denoise.py
+++ neural_priors/glm/fit_single_trials_denoise.py
@@ -13,14 +13,14 @@ warnings.filterwarnings('ignore')
 
 def main(subject, session, bids_folder, confounds=False, smoothed=False):
 
+    session = None if session == 0 else session
+
     derivatives = op.join(bids_folder, 'derivatives')
 
     sub = Subject(subject, bids_folder=bids_folder)
+
     runs = sub.get_runs(session)
-
     ims = sub.get_preprocessed_bold(session=session)
-
-    print(ims)
 
     base_dir = 'glm_stim1.denoise'
 
@@ -31,12 +31,6 @@ def main(subject, session, bids_folder, confounds=False, smoothed=False):
 
     data = [image.load_img(im).get_fdata() for im in ims]
 
-    base_dir = op.join(derivatives, base_dir, f'sub-{subject}',
-                       f'ses-{session}', 'func')
-
-    if not op.exists(base_dir):
-        os.makedirs(base_dir)
-
     onsets = sub.get_onsets(session)
     onsets['trial_type'] = onsets.apply(lambda row: f'stimulus_{row["n"]}' if row['trial_type'] == 'stimulus' else f'response_{row.response}', axis=1)
     onsets['duration'] = 0.0
@@ -46,20 +40,41 @@ def main(subject, session, bids_folder, confounds=False, smoothed=False):
     frametimes = np.linspace(tr/2., (n - .5)*tr, n)
     onsets['onset'] = ((onsets['onset']+tr/2.) // 2.3) * 2.3
 
-    dm = [make_first_level_design_matrix(frametimes, onsets.loc[run], hrf_model='fir', oversampling=100.,
-                                         drift_order=0,
-                                         drift_model=None).drop('constant', axis=1) for run in runs]
+    if session is None:
+        base_dir = op.join(derivatives, base_dir, f'sub-{subject}',
+                        'func')
+    else:
+        base_dir = op.join(derivatives, base_dir, f'sub-{subject}',
+                        f'ses-{session}', 'func')
 
-    dm = pd.concat(dm, keys=runs, names=['run']).fillna(0)
+        onsets = pd.concat([onsets], keys=[session], names=['session'])
+
+    if not op.exists(base_dir):
+        os.makedirs(base_dir)
+
+    dm = onsets[['onset', 'trial_type', 'duration']].groupby(['session', 'run']).apply(lambda d: make_first_level_design_matrix(frametimes, d, hrf_model='fir', drift_model=None, drift_order=0).drop('constant', axis=1)).fillna(0.0)    # dm = [make_first_level_design_matrix(frametimes, on, hrf_model='fir', oversampling=100.,
+    #                                      drift_order=0,
+    #                                      drift_model=None).drop('constant', axis=1) for (session, run), on in onsets.groupby(['session', 'run'])]
+
+    # dm = pd.concat(dm, keys=[(session, run) for (session, run), names=['run']).fillna(0)
     dm.columns = [c.replace('_delay_0', '') for c in dm.columns]
     dm /= dm.max()
+    dm = np.round(dm)
     print(dm)
+    print(dm.shape)
 
-    X = [dm.loc[run].values for run in runs]
+    X = [d.values for (session, run), d in dm.groupby(['session', 'run'])]
+    print(X)
+
+    for x in X:
+        print(x.shape)
 
     # create a directory for saving GLMsingle outputs
 
     opt = dict()
+
+    # opt['sessionindicator'] = np.array([session for (session, run), d in dm.groupby(['session', 'run'])])[np.newaxis, :]
+    # print(opt['sessionindicator'])
 
     # set important fields for completeness (but these would be enabled by default)
     opt['wantlibrary'] = 1
@@ -98,12 +113,10 @@ def main(subject, session, bids_folder, confounds=False, smoothed=False):
     r2.to_filename(op.join(base_dir, f'sub-{subject}_ses-{session}_task-task_space-T1w_desc-R2.nii.gz'))
 
 
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('subject', default=None)
-    parser.add_argument('session', default=None)
+    parser.add_argument('session', default=None, type=int)
     parser.add_argument('--bids_folder', default='/data/ds-neuralpriors')
     parser.add_argument('--smoothed', action='store_true')
     args = parser.parse_args()
