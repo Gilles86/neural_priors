a06390c9d6d6341e08942a880833982042d5c043
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-02-11 10:42:39 +0100

    Script to run MRIQC on all data

--- neural_priors/cluster_preproc/mriqc.sh
+++ neural_priors/cluster_preproc/mriqc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#
 #SBATCH --job-name=mriqc
-#SBATCH --output=/home/cluster/gdehol/logs/res_mriqc_%A-%a.txt
-#SBATCH --partition=generic
+#SBATCH --output=/home/gdehol/logs/res_mriqc_%A-%a.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
-#SBATCH --time=3:00:00
+#SBATCH --mem=96G
+#SBATCH --time=45:00
+source /etc/profile.d/lmod.sh
+module load singularityce/3.10.2
 export SINGULARITYENV_FS_LICENSE=$FREESURFER_HOME/license.txt
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
-singularity run --cleanenv /data/gdehol/containers/mriqc-0.16.1.simg /scratch/gdehol/ds-tmsrisk scratch/gdehol/ds-tmsrisk/derivatives/mriqc participant --participant-label $PARTICIPANT_LABEL  --verbose-reports -w /scratch/gdehol/work
+singularity run -B /shares/zne.uzh/containers/templateflow:/opt/templateflow -B /shares/zne.uzh/gdehol/ds-neuralpriors:/data -B /scratch/gdehol:/workflow --cleanenv /shares/zne.uzh/containers/mriqc-24.0.0 /data /data/derivatives/mriqc participant --participant_label $PARTICIPANT_LABEL -w /workflow --no-sub
