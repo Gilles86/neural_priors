1cc76ac7035dd657fb35f074c62bae54dfb1020d
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-02-11 12:10:43 +0100

    Heavy cleanup of encoding models

--- neural_priors/encoding_model/decode_across_batch.py
+++
@@ -1,7 +0,0 @@
-from neural_priors.utils.data import get_all_subject_ids
-from itertools import product
-from decode_across import main
-
-# for subject_id, smoothed, range_n in product(get_all_subject_ids()[:1], [False, True], ['narrow', 'wide']):
-for subject_id, smoothed, range_n in product(['24'], [False, True], ['narrow', 'wide']):
-    main(subject_id, range_n, smoothed, '/data/ds-neuralpriors', False)
