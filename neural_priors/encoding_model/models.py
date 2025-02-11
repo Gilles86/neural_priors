2c16e322cca50fc4713bfea794d0674f8b1d634f
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-02-11 14:42:52 +0100

    Model 1 and 2 implemented

--- neural_priors/encoding_model/models.py
+++ neural_priors/encoding_model/models.py
@@ -9,7 +9,7 @@ import pandas as pd
 def get_paradigm(sub, model_label):
     behavior = sub.get_behavioral_data(session=None)
 
-    if model_label in [1]:
+    if model_label in [1, 2]:
         paradigm = behavior[['n', 'range']].rename(columns={'n':'x'})
         paradigm['range'] = (paradigm['range'] == 'wide')
         paradigm = paradigm.astype(np.float32)
@@ -21,6 +21,8 @@ def get_paradigm(sub, model_label):
 def get_model(paradigm, model_label, gaussian=True):
 
     if model_label == 1:
+        regressors = {}
+    elif model_label == 2:
         regressors = {'mu':'0 + C(range)', 'sd':'0 + C(range)', 'amplitude':'0 + C(range)', 'baseline':'0 + C(range)'}
     else:
         raise NotImplementedError("Only model 1 is implemented")
@@ -36,14 +38,28 @@ def get_model(paradigm, model_label, gaussian=True):
 def fit_model(model, paradigm, data, model_label, max_n_iterations=1000):
 
     if model_label in [1]:
-        modes = np.linspace(5, 45, 5, dtype=np.float32)
-        sigmas = np.linspace(1, 60, 5, dtype=np.float32)
+        modes = np.linspace(5, 45, 41, dtype=np.float32)
+        sigmas = np.linspace(1, 30, 30, dtype=np.float32)
+        amplitudes = np.array([1.], dtype=np.float32)
+        baselines = np.array([0], dtype=np.float32)
+    elif model_label in [1, 2]:
+        modes = np.linspace(5, 45, 15, dtype=np.float32)
+        sigmas = np.linspace(1, 30, 15, dtype=np.float32)
         amplitudes = np.array([1.], dtype=np.float32)
         baselines = np.array([0], dtype=np.float32)
     
     optimizer = ParameterFitter(model, data.astype(np.float32), paradigm.astype(np.float32))
 
     if model_label == 1:
+        grid_pars = optimizer.fit_grid(modes, 
+                                       sigmas, 
+                                        amplitudes, 
+                                        baselines)
+        fixed_pars = list(model.parameter_labels)
+        fixed_pars.pop(fixed_pars.index(('amplitude_unbounded', 'Intercept')))
+        fixed_pars.pop(fixed_pars.index(('baseline_unbounded', 'Intercept')))
+
+    elif model_label == 2:
         grid_pars = optimizer.fit_grid(modes, modes,
                                        sigmas, sigmas,
                                         amplitudes, amplitudes,
