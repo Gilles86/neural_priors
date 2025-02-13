70fef23b5ef9074b7b3ad5f2b75a8bb6c77ab5ed
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-02-13 11:55:50 +0100

    Rewrote sampling code for new models

--- neural_priors/surface/sample_prf_to_surface_nilearn.py
+++ neural_priors/surface/sample_prf_to_surface_nilearn.py
@@ -21,46 +21,49 @@ def transform_fsaverage(in_file, fs_hemi, source_subject, bids_folder):
         r = sxfm.run()
         return r
 
-def main(subject, session, bids_folder, smoothed):
+def main(subject, model_label, bids_folder, smoothed, gaussian=True, keys_to_extract=['cvr2']):
 
-    session = None if session == 0 else session
-    
     sub = Subject(subject, bids_folder=bids_folder)
     surfinfo = sub.get_surf_info()
 
-    par_keys = ['mode', 'fwhm', 'amplitude', 'baseline', 'cvr2', 'r2']
+    prf_pars_volume = sub.get_prf_parameters_volume(model_label=model_label, smoothed=smoothed, return_image=True, cross_validated=False) 
 
-    for range_n in [None, 'wide', 'narrow']:
-        prf_pars_volume = sub.get_prf_parameters_volume(session, smoothed=smoothed, keys=par_keys, return_image=True, cross_validated=False,
-                                                        range_n=range_n)
+    key = 'encoding_model'
 
-        key = 'encoding_model.denoise'
+    key += f'.model{model_label}'
 
-        if smoothed:
-            key += '.smoothed'
-        
-        if range_n is not None:
-            key += f'.range_{range_n}'
+    if gaussian:
+        key += '.gaussian'
+    else:
+        raise NotImplementedError
 
-        if session is None:
-            target_dir = op.join(bids_folder, 'derivatives', key, f'sub-{subject}', 'func')
-        else:
-            target_dir = op.join(bids_folder, 'derivatives', key, f'sub-{subject}', f'ses-{session}', 'func')
+    if smoothed:
+        key += '.smoothed'
 
-        print(f'Writing to {target_dir}')
+    target_dir = op.join(bids_folder, 'derivatives', key, f'sub-{subject}', 'func')
 
-        for hemi in ['L', 'R']:
-            samples = surface.vol_to_surf(prf_pars_volume, surfinfo[hemi]['outer'], inner_mesh=surfinfo[hemi]['inner'])
-            samples = samples.astype(np.float32)
-            fs_hemi = 'lh' if hemi == 'L' else 'rh'
+    print(f'Writing to {target_dir}')
 
-            for ix, par in enumerate(par_keys):
+    if gaussian:
+        par_keys = ['mu.narrow', 'mu.wide',
+                    'sd.narrow', 'sd.wide',
+                    'amplitude.narrow', 'amplitude.wide',
+                    'baseline.narrow', 'baseline.wide',
+                    'r2', 'cvr2']
+
+    if keys_to_extract is None:
+        keys_to_extract = par_keys
+
+    for hemi in ['L', 'R']:
+        samples = surface.vol_to_surf(prf_pars_volume, surfinfo[hemi]['outer'], inner_mesh=surfinfo[hemi]['inner'])
+        samples = samples.astype(np.float32)
+        fs_hemi = 'lh' if hemi == 'L' else 'rh'
+
+        for ix, par in enumerate(par_keys):
+            if par in keys_to_extract:
                 im = nb.gifti.GiftiImage(darrays=[nb.gifti.GiftiDataArray(samples[:, ix])])
                 
-                if session is None:
-                    target_fn =  op.join(target_dir, f'sub-{subject}_desc-{par}.optim.nilearn_space-fsnative_hemi-{hemi}.func.gii')
-                else:
-                    target_fn =  op.join(target_dir, f'sub-{subject}_ses-{session}_desc-{par}.optim.nilearn_space-fsnative_hemi-{hemi}.func.gii')
+                target_fn =  op.join(target_dir, f'sub-{subject}_desc-{par}.optim.nilearn_space-fsnative_hemi-{hemi}.func.gii')
 
                 nb.save(im, target_fn)
 
@@ -69,9 +72,9 @@ def main(subject, session, bids_folder, smoothed):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('subject', default=None)
-    parser.add_argument('session', default=None, type=int)
+    parser.add_argument('model_label', default=1, type=int)
     parser.add_argument('--bids_folder', default='/data/ds-neuralpriors')
     parser.add_argument('--smoothed', action='store_true')
     args = parser.parse_args()
 
-    main(args.subject, args.session, bids_folder=args.bids_folder, smoothed=args.smoothed)
+    main(args.subject, args.model_label, bids_folder=args.bids_folder, smoothed=args.smoothed)
