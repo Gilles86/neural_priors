61e62a524cae11b5def450742ce1493a5ac77daf
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-03-13 16:15:02 +0100

    Model code for advanced Alpha models

--- neural_priors/encoding_model/models.py
+++ neural_priors/encoding_model/models.py
@@ -4,6 +4,10 @@ from braincoder.optimize import ParameterFitter
 import os.path as op
 import numpy as np
 import pandas as pd
+from braincoder.models import AlphaGaussianPRF
+import tensorflow as tf
+import tensorflow_probability as tfp
+import logging
 
 '''
 1. 4-parameter model (everything same)
@@ -33,6 +37,10 @@ def get_paradigm(sub, model_label, gaussian=True):
 
         if not gaussian:
             paradigm['x'] = np.log(paradigm['x'])
+    elif model_label in [12, 13]:
+        paradigm = behavior[['n', 'range']].rename(columns={'n':'x'})
+        paradigm['range'] = paradigm['range'].map({'narrow':False, 'wide':True})
+        paradigm = paradigm[['x', 'range']]
     else:
         raise NotImplementedError(f"Model {model_label} is not implemented")
 
@@ -73,6 +81,8 @@ def get_regressors(model_label):
         regressors = {'sd':'0 + beta'}
     elif model_label == 11:
         regressors = {'mu':'0 + C(range)', 'sd':'0 + C(range)'}
+    elif model_label in [12, 13]:
+        return None
     else:
         raise NotImplementedError(f"Model {model_label} is not implemented")
 
@@ -195,6 +205,24 @@ def get_conditionspecific_parameters(model_label, model, estimated_parameters, g
     range_increase = range_increase_log_space if not gaussian else range_increase_natural_space
 
     print("Getting parameters with range_increase", range_increase)
+
+
+    if model_label in [12, 13]:
+
+        pars = pd.DataFrame()
+
+        pars[('mu', 'narrow')] = estimated_parameters['mu_narrow']
+        pars[('mu', 'wide')] = estimated_parameters['delta_wide'] * (estimated_parameters['mu_narrow'] - estimated_parameters['lower_bound_range']) + estimated_parameters['lower_bound_range']
+
+        for p in ['sd', 'amplitude', 'baseline', 'alpha', 'delta_wide', 'lower_bound_range']:
+            pars[(p, 'narrow')] = estimated_parameters[p]
+            pars[(p, 'wide')] = estimated_parameters[p]
+
+        pars.columns = pd.MultiIndex.from_tuples(pars.columns, names=['parameter', 'range'])
+        
+        return pars.stack('range').reorder_levels(['range', 'source'], axis=0).sort_index()
+
+
     
     if model_label in [1,2, 6, 7, 8, 9, 11]:
         conditions = pd.DataFrame({'x':[0,0], 'range':[0,1]}, index=pd.Index(['narrow', 'wide'], name='range'))
@@ -208,3 +236,78 @@ def get_conditionspecific_parameters(model_label, model, estimated_parameters, g
     pars = model.get_conditionspecific_parameters(conditions, estimated_parameters)
 
     return pars
+
+
+
+class AlphaDeltaModel(AlphaGaussianPRF):
+
+    parameter_labels = ['mu_narrow', 'sd', 'alpha', 'delta_wide', 'lower_bound_range', 'amplitude', 'baseline']
+
+    def __init__(self, paradigm=None, data=None, parameters=None,
+                 weights=None, omega=None, allow_neg_amplitudes=False, verbosity=logging.INFO,
+                 model_stimulus_amplitude=False,
+                 **kwargs):
+
+        if allow_neg_amplitudes:
+            raise NotImplementedError("Negative amplitudes are not allowed for AlphaDeltaModel")
+
+        super().__init__(paradigm=paradigm, data=data, parameters=parameters,
+                         weights=weights, omega=omega, allow_neg_amplitudes=allow_neg_amplitudes,
+                          verbosity=verbosity, model_stimulus_amplitude=model_stimulus_amplitude,
+                          **kwargs)
+    @tf.function
+    def _transform_parameters_forward2(self, parameters):
+        return tf.concat([tf.math.softplus(parameters[:, 0][:, tf.newaxis]),
+                          tf.math.softplus(parameters[:, 1][:, tf.newaxis]),
+                          parameters[:, 2][:, tf.newaxis],
+                          parameters[:, 3][:, tf.newaxis],
+                          parameters[:, 4][:, tf.newaxis],
+                          tf.math.softplus(parameters[:, 5][:, tf.newaxis]), # Amplitude
+                          parameters[:, 6][:, tf.newaxis]], axis=1)
+    
+    @tf.function
+    def _transform_parameters_backward2(self, parameters):
+        return tf.concat([tfp.math.softplus_inverse(parameters[:, 0][:, tf.newaxis]),
+                          tfp.math.softplus_inverse(
+                              parameters[:, 1][:, tf.newaxis]),
+                          parameters[:, 2][:, tf.newaxis],
+                          parameters[:, 3][:, tf.newaxis],
+                          parameters[:, 4][:, tf.newaxis],
+                          tfp.math.softplus_inverse(parameters[:, 5][:, tf.newaxis]),
+                          parameters[:, 6][:, tf.newaxis]], axis=1)
+
+    @tf.function
+    def _basis_predictions_without_amplitude(self, paradigm, parameters):
+
+        def alpha_transform(x, alpha, eps=1e-6):
+            """ Computes a numerically stable alpha transformation. """
+            return tf.where(
+                tf.abs(alpha) < eps,
+                tf.math.log(x),  # Directly use log(x) when alpha ≈ 0
+                (tf.pow(x, alpha) - 1) / alpha
+            )
+
+        def f_x(x, mu_x, sigma_mu, alpha):
+            """ Computes p_x(x | mu_x, sigma_x) using the given formula. """
+            mu_alpha_x = alpha_transform(x, alpha)  # Using your transformation
+            mu_alpha_mu = alpha_transform(mu_x, alpha)  # Using your transformation
+            exponent = -tf.square(mu_alpha_x - mu_alpha_mu) / (2 * tf.square(sigma_mu))
+            return tf.exp(exponent)
+
+        # Extract stimulus feature values
+        x = paradigm[..., tf.newaxis, 0]
+        wide_condition = tf.cast(paradigm[..., 1], tf.bool)  # Ensure this is a tensor
+
+        delta_wide = parameters[..., 3]
+        lower_bound_range = parameters[..., 4]
+
+        mu_narrow = parameters[..., 0]
+        mu_wide = ((mu_narrow - lower_bound_range) * delta_wide) + lower_bound_range
+
+        mu = tf.where(tf.transpose(wide_condition), mu_wide, mu_narrow)[tf.newaxis, ...]
+
+        return f_x(x,
+                   mu,
+                    parameters[:, tf.newaxis, :, 1],
+                    parameters[:, tf.newaxis, :, 2]) * \
+            parameters[:, tf.newaxis, :, 5] + parameters[:, tf.newaxis, :, 6]
