395cf037b6700b7cf764bb1e047ee5be5c2964bb
Gilles de Hollander <gilles.de.hollander@gmail.com>
2026-02-26 11:36:46 +0100

    Fixed scaling jobs

---
+++ neural_priors/encoding_model/slurm_jobs/submit_fixed_sd_scaling_jobs.sh
@@ -0,0 +1,9 @@
+for MODEL in {115..145}; do
+    # ROI fits
+    sbatch --array=${ARRAY} "${SCRIPT_DIR}/fit_model.sh"           ${MODEL} --smoothed
+    sbatch --array=${ARRAY} "${SCRIPT_DIR}/fit_model.sh"           ${MODEL} --smoothed --fit_responses
+
+    sbatch --array=${ARRAY} "${SCRIPT_DIR}/fit_model_cv.sh"           ${MODEL} --smoothed
+    sbatch --array=${ARRAY} "${SCRIPT_DIR}/fit_model_cv.sh"           ${MODEL} --smoothed --fit_responses
+
+done
