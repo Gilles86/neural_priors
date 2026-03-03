414c06218101ab253e32debf83acf8d8b0986db9
Gilles de Hollander <gilles.de.hollander@gmail.com>
2026-03-03 14:05:49 +0100

    Changed directory of decoding script

--- neural_priors/encoding_model/slurm_jobs/decode.sh
+++ neural_priors/encoding_model/slurm_jobs/decode.sh
@@ -49,7 +49,7 @@ done
 LOGFILE="/home/gdehol/logs/decode_neural_priors_${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}_model-${MODEL}_${SMOOTHED_SUFFIX}_${FIT_RESPONSES_SUFFIX}_${SPHERICAL_SUFFIX}_${SEPARATE_SIGMAS_SUFFIX}.txt"
 
 # Run the encoding model fit and redirect output manually
-python $HOME/git/neural_priors/neural_priors/encoding_model2/decode.py \
+python $HOME/git/neural_priors/neural_priors/encoding_model/decode.py \
     $PARTICIPANT_LABEL --bids_folder /shares/zne.uzh/gdehol/ds-neuralpriors \
     --n_voxels $N_VOXELS \
     $SMOOTHED_FLAG $FIT_RESPONSES_FLAG $SPHERICAL_FLAG $SEPARATE_SIGMAS_FLAG --model_label $MODEL > "$LOGFILE" 2>&1
